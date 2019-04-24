f7a3300f4769efaf47992d01209d8965b19c1db3
vagrant <vagrant@homestead>
2019-04-24 02:24:12 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','Sample-关于页')
+@section('content')
+<h1>关于页</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+
+@section('content')
+<h1>主页</h1>
+@section('title','Sample-laravel新手入门教程')
+@stop
